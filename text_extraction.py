428565cc0aff6f9bf2c3c60a9bb388e48515e835
axelle <inezaaxelle44@gmail.com>
2024-02-11 22:24:30 +0200

    removed wrong image

--- text_extraction.py
+++ text_extraction.py
@@ -6,7 +6,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # Load image, convert to grayscale, sharpen, apply Otsu's threshold
-image = cv2.imread('img.jpg')
+image = cv2.imread('image.png')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 sharpened_image = cv2.filter2D(gray_image, -1, sharpen_kernel)
